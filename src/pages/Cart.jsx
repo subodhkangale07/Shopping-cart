55d18367a785a89b0df1a89ddb244fe21c2e9a1f
Subodh kangale <125494916+subodhkangale07@users.noreply.github.com>
2024-07-30 09:31:48 +0530

    first commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -46,12 +46,16 @@ const Cart = () => {
                    
                 ):
                 (
-                    <div>
-                        <h1>Cart Empty</h1>
-                        <NavLink to={"/"}>
-                            <button>Shopnow</button>
-                        </NavLink>
-                        </div>
+                    <div className="min-h-[80vh] flex flex-col items-center justify-center">
+                    <h1 className="text-gray-700 font-semibold text-xl mb-2">
+                      Your cart is empty!
+                    </h1>
+                    <NavLink to={"/"}>
+                      <button className="uppercase bg-green-600 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-semibold hover:text-green-700 p-3 px-10 tracking-wider">
+                        Shop Now
+                      </button>
+                    </NavLink>
+                  </div>
                 )
             }
 
